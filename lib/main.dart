15d186e9b3fbbc89da03519340f90b4933c30d7d
sujiewei_git <925604487@qq.com>
2019-08-10 01:20:26 +0800

    添加打包apk相关配置

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,51 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Home(),
+      theme: ThemeData(
+          primarySwatch: Colors.yellow
+      ),
+      home: Homo(context),
     );
   }
+
+  Scaffold Homo(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text(
+          'SUJW'
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: _ListItemBuilder),
+    );
+  }
+
+  Widget _ListItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          Image.network(list[index].imageUrl),
+          SizedBox(height: 5,),
+          Text(
+            list[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            list[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 5,)
+        ],
+      ),
+    );
+  }
+
 }
 
 class Home extends StatelessWidget{
@@ -24,7 +66,7 @@ class Home extends StatelessWidget{
       home: Scaffold(
         backgroundColor: Colors.grey[200],
         appBar: AppBar(
-          elevation: 12,
+          elevation: 0.0,
           title: Text('SUJW'),
         ),
         body: ListView.builder(
@@ -57,7 +99,6 @@ class Home extends StatelessWidget{
     );
   }
 }
-
 
 class Holle extends StatelessWidget {
   @override
